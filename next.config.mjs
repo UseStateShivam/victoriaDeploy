26666f321879c251437351bcb2a8430b50a750b9
Shivam Verma <mcrafter147@gamil.com>
2024-06-29 20:40:41 +0530

    ii

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 // next.config.mjs
 
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     distDir: 'dist',
+    basePath: isProd ? '/victoriaDeploy' : '',
     output: 'export',
     images: {
         unoptimized: true,
